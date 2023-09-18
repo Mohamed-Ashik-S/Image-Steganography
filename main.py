05becba1bd5d8c98330d9166df09e5e49b242a6a
Mohamed_Ashik <114716980+Mohamed-Ashik-S@users.noreply.github.com>
2023-09-18 18:41:18 +0530

    Update main.py
    Added a new Pass feature, which enhances the protection of the encoded message

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
 
+# Password for encoding and decoding
+password = ""
+
 def encode_lsb(image_path, secret_message, output_path):
+    global password  # Access the global password variable
+
     # Load the image
     img = Image.open(image_path)
     img = img.convert("RGB")
@@ -38,6 +43,8 @@ def encode_lsb(image_path, secret_message, output_path):
     print("Message encoded successfully in the image.")
 
 def decode_lsb(image_path):
+    global password  # Access the global password variable
+
     img = Image.open(image_path)
     img = img.convert("RGB")
 
@@ -56,20 +63,19 @@ def decode_lsb(image_path):
     padded_length = (len(binary_message) + 7) // 8 * 8
     binary_message = binary_message.ljust(padded_length, "0")
 
-    return binary_message
+    # Extract the secret message
+    secret_message = ''.join(chr(int(binary_message[i:i + 8], 2)) for i in range(0, len(binary_message), 8))
 
-def Binary_to_text(binary_string):
-    text = ""
-    padded_binary = binary_string.ljust((len(binary_string) + 7) // 8 * 8, "0")  # Pad binary message to be a multiple of 8
-    for i in range(0, len(padded_binary), 8):
-        byte = padded_binary[i:i + 8]
-        text += chr(int(byte, 2))
-    return text
+    return secret_message
 
 def open_image():
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
     if file_path:
         image_path_var.set(file_path)
+
+def set_password():
+    global password
+    password = password_entry.get()
 
 def encode_message():
     image_path = image_path_var.get()
@@ -86,10 +92,16 @@ def decode_message():
     image_path = image_path_var.get()
     try:
         decoded_message = decode_lsb(image_path)
-        decoded_text = Binary_to_text(decoded_message)
-        decoded_message_var.set(decoded_text)
-        status_var.set("Message decoded successfully!")
-        print("Decoded message:", decoded_message)  # Print the decoded message in the terminal
+        entered_pin = pin_entry.get()
+        if password:  # If password is set, use it for decoding
+            if entered_pin == password:  # Check if the entered PIN matches the password
+                decoded_message_var.set(decoded_message)
+                status_var.set("Message decoded successfully!")
+            else:
+                decoded_message_var.set("Error: Wrong PIN.")
+        else:  # If no password is set, display the secret message directly
+            decoded_message_var.set(decoded_message)
+            status_var.set("Message decoded successfully!")
     except Exception as e:
         status_var.set(f"Error: {e}")
 
@@ -108,20 +120,34 @@ tk.Label(root, text="Secret Message:").grid(row=1, column=0)
 secret_message_entry = tk.Entry(root)
 secret_message_entry.grid(row=1, column=1)
 
+# Password entry
+tk.Label(root, text="Password:").grid(row=2, column=0)
+password_entry = tk.Entry(root, show='*')  # Mask the password with asterisks
+password_entry.grid(row=2, column=1)
+
+# Set Password button
+tk.Button(root, text="Set Password", command=set_password).grid(row=2, column=2)
+
 # Encode button
-tk.Button(root, text="Encode", command=encode_message).grid(row=2, column=0)
+tk.Button(root, text="Encode", command=encode_message).grid(row=3, column=0)
 
 # Decode button
-tk.Button(root, text="Decode", command=decode_message).grid(row=2, column=1)
+tk.Button(root, text="Decode", command=decode_message).grid(row=3, column=1)
 
 # Decoded message display
 decoded_message_var = tk.StringVar()
-tk.Label(root, text="Decoded Message:").grid(row=3, column=0)
-tk.Label(root, textvariable=decoded_message_var, wraplength=300, anchor="w", justify="left").grid(row=3, column=1)
+tk.Label(root, text="Decoded Message:").grid(row=4, column=0)
+tk.Label(root, textvariable=decoded_message_var, wraplength=300, anchor="w", justify="left").grid(row=4, column=1)
+
+# PIN entry
+tk.Label(root, text="Enter PIN:").grid(row=5, column=0)
+pin_entry = tk.Entry(root)
+pin_entry.grid(row=5, column=1)
 
 # Status label
 status_var = tk.StringVar()
 status_label = tk.Label(root, textvariable=status_var, fg="red")
-status_label.grid(row=4, column=0, columnspan=3)
+status_label.grid(row=6, column=0, columnspan=3)
+
 
 root.mainloop()
