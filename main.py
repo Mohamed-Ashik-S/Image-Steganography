2324957305ada9cdf5412b5ce1663426552308d3
Mohamed_Ashik <114716980+Mohamed-Ashik-S@users.noreply.github.com>
2023-09-25 18:53:31 +0530

    Update main.py
    Added Pass Feature

--- main.py
+++ main.py
@@ -2,26 +2,23 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
 
-# Password for encoding and decoding
-password = ""
-
-def encode_lsb(image_path, secret_message, output_path):
-    global password  # Access the global password variable
-
+def encode_lsb(image_path, secret_message, output_path, pin):
     # Load the image
     img = Image.open(image_path)
     img = img.convert("RGB")
-
+    # pin = decimal_to_binary(pin)
     # Get the binary representation of the secret message
     binary_secret_message = ''.join(format(ord(c), "08b") for c in secret_message)
-
+    pin = ''.join(format(ord(c), "08b") for c in pin)
+    binary_secret_message += pin
     # Check if the image can hold the secret message
     if len(binary_secret_message) > img.size[0] * img.size[1] * 3:
         raise ValueError("Image too small to hold the message. Try using a larger image or a shorter message.")
 
     # Add termination delimiter to the binary message
     binary_secret_message += "00000000"
-
+        # Pad with zeros until we have a multiple of eight bits
+    print(binary_secret_message)
     # Counter variable to keep track of the binary message index
     message_index = 0
     message_bytes = bytes([int(binary_secret_message[i:i + 8], 2) for i in range(0, len(binary_secret_message), 8)])
@@ -43,8 +40,6 @@ def encode_lsb(image_path, secret_message, output_path):
     print("Message encoded successfully in the image.")
 
 def decode_lsb(image_path):
-    global password  # Access the global password variable
-
     img = Image.open(image_path)
     img = img.convert("RGB")
 
@@ -54,36 +49,50 @@ def decode_lsb(image_path):
             pixel = img.getpixel((x, y))
             for c in range(3):
                 binary_message += format(pixel[c], "08b")[-1]  # Extract the LSB of each color channel
-
+    # # Split the binary string into a list of 8-bit elements
     # Find the end of the message (it is terminated with eight 0 bits)
-    end_index = binary_message.find("00000000")
-    binary_message = binary_message[:end_index]
-
-    # Pad binary message to be a multiple of 8
+    binary_list = [binary_message[i:i+8] for i in range(0, len(binary_message), 8)]
+    # rint(binary_list.index("00000000")  * 8)
+    end_index = binary_list.index("00000000") * 8 
+    combined_binary = binary_message[:end_index]
+    extracted_pin = combined_binary[-32:]
+    binary_message = combined_binary[:len(combined_binary)-len(extracted_pin)]
+    print("binary_message:",binary_message)
+    print("orginal_pin:",extracted_pin)
+    #Pad binary message to be a multiple of 8
     padded_length = (len(binary_message) + 7) // 8 * 8
     binary_message = binary_message.ljust(padded_length, "0")
+    
+    return binary_message, extracted_pin
 
-    # Extract the secret message
-    secret_message = ''.join(chr(int(binary_message[i:i + 8], 2)) for i in range(0, len(binary_message), 8))
-
-    return secret_message
+def Binary_to_text(binary_string):
+    text = ""
+    padded_binary = binary_string.ljust((len(binary_string) + 7) // 8 * 8, "0")  # Pad binary message to be a multiple of 8
+    for i in range(0, len(padded_binary), 8):
+        byte = padded_binary[i:i + 8]
+        text += chr(int(byte, 2))
+    return text
 
 def open_image():
     file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
     if file_path:
         image_path_var.set(file_path)
 
-def set_password():
-    global password
-    password = password_entry.get()
+# def decimal_to_binary(decimal_value):
+#     binary_str = bin(decimal_value)[2:]
+#     binary_str = "0" * (16 - len(binary_str)) + binary_str
+#     return binary_str
+
+
 
 def encode_message():
     image_path = image_path_var.get()
     secret_message = secret_message_entry.get()
+    pin = encode_Pin_entry.get()
     output_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
     if output_path:
         try:
-            encode_lsb(image_path, secret_message, output_path)
+            encode_lsb(image_path, secret_message, output_path, pin)
             status_var.set("Message encoded successfully!")
         except Exception as e:
             status_var.set(f"Error: {e}")
@@ -91,17 +100,18 @@ def encode_message():
 def decode_message():
     image_path = image_path_var.get()
     try:
-        decoded_message = decode_lsb(image_path)
-        entered_pin = pin_entry.get()
-        if password:  # If password is set, use it for decoding
-            if entered_pin == password:  # Check if the entered PIN matches the password
-                decoded_message_var.set(decoded_message)
-                status_var.set("Message decoded successfully!")
-            else:
-                decoded_message_var.set("Error: Wrong PIN.")
-        else:  # If no password is set, display the secret message directly
-            decoded_message_var.set(decoded_message)
+        decoded_message, orginal_pin = decode_lsb(image_path)
+        entered_pin = Decode_pin_entry.get()
+        entered_pin = ''.join(format(ord(c), "08b") for c in entered_pin)
+        print("ENTERED:", entered_pin)
+        if entered_pin == orginal_pin:
+            decoded_text = Binary_to_text(decoded_message)
+            decoded_message_var.set(decoded_text)
             status_var.set("Message decoded successfully!")
+            print("Decoded message:", decoded_message)  # Print the decoded message in the terminal
+        else:
+            decoded_message_var.set("")
+            status_var.set("Error: Incorrect PIN")
     except Exception as e:
         status_var.set(f"Error: {e}")
 
@@ -120,13 +130,10 @@ tk.Label(root, text="Secret Message:").grid(row=1, column=0)
 secret_message_entry = tk.Entry(root)
 secret_message_entry.grid(row=1, column=1)
 
-# Password entry
-tk.Label(root, text="Password:").grid(row=2, column=0)
-password_entry = tk.Entry(root, show='*')  # Mask the password with asterisks
-password_entry.grid(row=2, column=1)
-
-# Set Password button
-tk.Button(root, text="Set Password", command=set_password).grid(row=2, column=2)
+# PIN entry
+tk.Label(root, text="PIN(4-digit):").grid(row=2, column=0)
+encode_Pin_entry = tk.Entry(root, show='*')
+encode_Pin_entry.grid(row=2, column=1)
 
 # Encode button
 tk.Button(root, text="Encode", command=encode_message).grid(row=3, column=0)
@@ -140,14 +147,14 @@ tk.Label(root, text="Decoded Message:").grid(row=4, column=0)
 tk.Label(root, textvariable=decoded_message_var, wraplength=300, anchor="w", justify="left").grid(row=4, column=1)
 
 # PIN entry
-tk.Label(root, text="Enter PIN:").grid(row=5, column=0)
-pin_entry = tk.Entry(root)
-pin_entry.grid(row=5, column=1)
+tk.Label(root, text="Enter PIN For Decoding:").grid(row=5, column=0)
+Decode_pin_entry = tk.Entry(root)
+Decode_pin_entry.grid(row=5, column=1)
 
 # Status label
 status_var = tk.StringVar()
 status_label = tk.Label(root, textvariable=status_var, fg="red")
 status_label.grid(row=6, column=0, columnspan=3)
 
-
 root.mainloop()
+
